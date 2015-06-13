8a66d3533b12cddff58d0e1e9fcd296129100dcc
Daniel Treasure <dbtreasure@gmail.com>
2015-06-13 13:47:53 -0600

    initial post with framework

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+angular.module('MyApp')
+
+.controller('AppCtrl', function($scope) {
+	$scope.imagePath = 'img/washedout.png';
+	$scope.messages = [{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }];
+});
