5ace5897e0b603203cc4c43c4a5e746ad459bb00
Daniel Treasure <dbtreasure@gmail.com>
2015-06-13 15:45:28 -0600

    adds gradient and stuff

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 
-angular.module('MyApp')
 
-.controller('AppCtrl', function($scope) {
+angular.module('MyApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('orange');
+}).
+ controller('AppCtrl', function($scope) {
 	$scope.imagePath = 'img/washedout.png';
 	$scope.messages = [{
       what: 'Brunch this weekend?',
@@ -28,5 +33,50 @@ angular.module('MyApp')
       who: 'Min Li Chan',
       when: '3:08PM',
       notes: " I'll be in your neighborhood doing errands"
-    }];
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },{
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    },];
 });
